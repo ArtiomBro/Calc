03439b433c6a123d181c06492b7d789f486a0ed2
pogudin-2021@bk.ru <160314494+ArtiomBro@users.noreply.github.com>
2024-04-12 22:18:33 +0400

    Я попытался сделать 4 базовые действия, но не получилось(

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -17,45 +17,62 @@ namespace calculator_c_sharp
             sign = Convert.ToChar(Console.ReadLine());
             Console.Write("Введите Второе число: ");
             two = Convert.ToSingle(Console.ReadLine());
-
-            if (sign == '+')
+        }
+        public void Doaddition()
+        {
+            static void Main(string[] args)
             {
-                result = one + two;
-                Console.WriteLine("Сумма ваших чисел равна " + result);
-                Console.WriteLine("Для выхода нажмите любую клавишу...");
-                Console.ReadKey();
-            }
-            else if (sign == '-')
-            {
-                result = one - two;
-                Console.WriteLine("Разность ваших чисел равна " + result);
-                Console.WriteLine("Для выхода нажмите любую клавишу...");
-                Console.ReadKey();
-            }
-            else if (sign == '*')
-            {
-                result = one * two;
-                Console.WriteLine("Произведение ваших чисел равно " + result);
-                Console.WriteLine("Для выхода нажмите любую клавишу...");
-                Console.ReadKey();
-            }
-            else if (sign == '/')
-            {
-                result = one / two;
-                Console.WriteLine("Частное ваших чисел равно " + result);
-                Console.WriteLine("Для выхода нажмите любую клавишу...");
-                Console.ReadKey();
-
-
-            }
-            else
-            {
-                Console.WriteLine("Ошибка. Вы ввели неверный знак.");
-                Console.WriteLine("Для выхода нажмите любую клавишу...");
-                Console.ReadKey();
-
-
+                if (sign == '+')
+                {
+                    result = one + two;
+                    Console.WriteLine("Сумма ваших чисел равна " + result);
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
             }
         }
+        public void Vichitan()
+        {
+            static void Main(string[] args)
+            {
+                if (sign == '-')
+                {
+                    result = one - two;
+                    Console.WriteLine("Разность ваших чисел равна " + result);
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+            }
+        }
+        public void Umnochenie()
+        {
+            static void Main(string[] args)
+            {
+                if (sign == '*')
+                {
+                    result = one * two;
+                    Console.WriteLine("Произведение ваших чисел равно " + result);
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+            }
+        }
+        public void Deleniye()
+        {
+            static void Main(string[] args)
+            {
+                if (sign == '/')
+                {
+                    result = one / two;
+                    Console.WriteLine("Частное ваших чисел равно " + result);
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка. Вы ввели неверный знак.");
+                    Console.WriteLine("Для выхода нажмите любую клавишу...");
+                    Console.ReadKey();
+                }
+        }
     }
-}
